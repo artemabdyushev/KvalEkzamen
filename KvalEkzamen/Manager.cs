3a82bab0330ed00f454909985f3fa3baeb24a07a
artemabdyushev <artemabdyushev228@yandex.ru>
2021-06-29 10:15:42 +0300

    projecting menu

---
+++ KvalEkzamen/Manager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using System.Windows.Controls;
+
+namespace KvalEkzamen
+{
+    public class Remains
+    {
+        public int id_remains { get; set; }
+        public int id_items { get; set; }
+        public DateTime dateTime { get; set; }
+        public int count_lost { get; set; }
+    }
+
+    public class Category
+    {
+        public int id_category { get; set; }
+        public string category_name { get; set; }
+    }
+
+    public class Items
+    {
+        public int id_item { get; set; }
+        public string item_name { get; set; }
+        public int category_number { get; set; }
+        public double cost { get; set; }
+        public string about_item { get; set; }
+        public double reate_index_item { get; set; }
+        public int id_producer { get; set; }
+    }
+
+    public class Producer
+    {
+        public int id_producer { get; set; }
+        public string name_producer { get; set; }
+        public string country_producer { get; set; }
+    }
+    class Manager
+    {
+        public static Frame frame;
+        public static readonly string connectionInfo = ConfigurationManager.ConnectionStrings["connectionSqlCheck"].ToString();
+    }
+}
---
+++ KvalEkzamen/Menu.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KvalEkzamen
+{
+    /// <summary>
+    /// Логика взаимодействия для Menu.xaml
+    /// </summary>
+    public partial class Menu : Page
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenItemsPage(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenCategoryPage(object sender, RoutedEventArgs e)
+        {
+            Manager.frame.Navigate(new CategoryPage());
+        }
+
+        private void OpenRemainsPage(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
